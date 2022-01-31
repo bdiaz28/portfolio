c547426842f5b76b5370f86c0c72e0eab2127fbf
bdiaz28 <brittanymdiaz@yahoo.com>
2022-01-31 15:06:20 -0500

    restructure folders

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
-import About from "./components/About";
+import About from "./components/Home";
 import Contact from "./components/Contact";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
+
+
 
 
 export default function App() {
