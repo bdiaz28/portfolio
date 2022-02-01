1f58f6cb57ba591a9c3df12cafdd9dd559980f9f
bdiaz28 <brittanymdiaz@yahoo.com>
2022-01-31 20:04:04 -0500

    homepage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Home from "./components/Home"
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Navbar from "./components/Navbar";
@@ -12,6 +13,7 @@ export default function App() {
   return (
     <main className="text-gray-400 bg-gray-900 body-font">
       <Navbar />
+      <Home />
       <About />
       <Projects />
       <Skills />
